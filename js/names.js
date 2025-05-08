484bf8f7711ca3fdfb73bebab644e4e47f9a60a0
Nicolapsq <nicolapasqua84@gmail.com>
2025-05-08 15:49:38 +0200

    Add-first-function-and-export

---
+++ js/names.js
@@ -0,0 +1,12 @@
+function objectNames(name, surname) {
+    return {
+        firstName: name,
+        lastName: surname
+    }
+};
+
+module.exports = {
+    objectNames: objectNames,
+};
+
+// module.exports = objectNames
